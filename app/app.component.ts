f5a4bdc5ddd0b039e54585615a5c8afd95188faa
Andre Henriques <andre.henriques@optimalstrategix.com>
2016-09-20 22:40:46 -0400

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>
+    My First Angular 2 App
+</h1>
+
+<h2>Super cool!</h2>`
+})
+
+export class AppComponent {
+}
