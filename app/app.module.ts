623185db7cdcf39fe67b9aaddefe529eaa031adf
Andre Henriques <andre.henriques@optimalstrategix.com>
2016-09-25 01:04:50 -0400

    4. multiple components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,20 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
+    bootstrap: [AppComponent],
+
 })
 
 export class AppModule {
---
+++ app/hero.js
@@ -0,0 +1,11 @@
+/**
+ * Created by andre on 9/25/16.
+ */
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
